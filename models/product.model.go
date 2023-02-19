baa56e0e5e388279bb4968897010d2fdca78bc85
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-19 15:39:32 +0500

    added product model

---
+++ models/product.model.go
@@ -0,0 +1,14 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Product struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	Price       float64            `json:"price,omitempty" bson:"price,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"`
+	Quantity    int                `json:"quantity,omitempty" bson:"quantity,omitempty"`
+	Category    string             `json:"category,omitempty" bson:"category,omitempty"`
+	Images      []string           `json:"images,omitempty" bson:"images,omitempty"`
+	Url         string             `json:"url,omitempty" bson:"url,omitempty"`
+}
